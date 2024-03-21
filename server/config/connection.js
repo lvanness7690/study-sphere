c43d77ca4c316bd6b939602193147ccaf011b4d9
lvanness7690 <121961344+lvanness7690@users.noreply.github.com>
2024-03-20 20:35:30 -0400

    updated server and connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/userdb');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studySphereDataBase');
 
 module.exports = mongoose.connection;
