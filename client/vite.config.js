079dbffdd33259befb13d3273fc91810aeca1d18
Kevin Pierce <kpierce236@gmail.com>
2024-03-25 21:00:53 -0400

    Recreated Vite App so the build command works

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // root: './client', // Remove or adjust this line
   plugins: [react()],
   server: {
     port: 3000,
@@ -12,7 +12,7 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
-      }
+      },
     }
-  },
-});
+  }
+})
